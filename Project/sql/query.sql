709010323826abad7346e8b0fe50f11ebd04da5b
Fatima Yagubova <yagubovafatima@gmail.com>
2021-02-20 11:55:32 +0400

    Database's tables are finished

--- Project/sql/query.sql
+++ Project/sql/query.sql
@@ -1,13 +1,10 @@
-CREATE TABLE Posts(
+CREATE TABLE Post_tag(
     id INTEGER PRIMARY KEY,
-    Post_cover_url VARCHAR(50),
-    Post_title VARCHAR(250),
-    Post_author VARCHAR(100),
-    Post_date DATE,
-    Post_read_min TIME,
-    Post_description TEXT,
-    Post_content TEXT,
-    Post_img_url VARCHAR(250),
-    FOREIGN KEY (Post_author)
-    REFERENCES Authors(id)
+    Post_id INTEGER,
+    Tag_id INTEGER,
+    FOREIGN KEY (Post_id)
+    REFERENCES Posts(id)
+    FOREIGN KEY (Tag_id)
+    REFERENCES Tag(id)
 )
+SELECT * FROM Post_tag
